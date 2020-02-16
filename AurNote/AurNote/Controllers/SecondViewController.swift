e6d63fe1f18b2eb895156887f4c3130273b29317
Ayush Patel <ayushp3435@gmail.com>
2020-02-16 15:09:29 -0800

    removed submodule and added correct project folder

---
+++ AurNote/AurNote/Controllers/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  AurNote
+//
+//  Created by Ayush Patel on 2/16/20.
+//  Copyright © 2020 Ayush Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("here")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
