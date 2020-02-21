f757cb8a4055ab6e03a12f1671c913579f1ba903
AashitaP <harshupatwari@gmail.com>
2020-02-20 22:23:00 -0800

    Added inline documentation

--- AurNote/AurNote/Models/Utilities.swift
+++ AurNote/AurNote/Models/Utilities.swift
@@ -11,6 +11,7 @@ import VideoToolbox
 extension CIImage {
     
     /// Returns a pixel buffer of the image's current contents.
+    /// - Parameter pixelFormat: Format of pixel
     func toPixelBuffer(pixelFormat: OSType) -> CVPixelBuffer? {
         var buffer: CVPixelBuffer?
         let options = [
@@ -33,6 +34,7 @@ extension CIImage {
     }
     
     /// Returns a copy of this image scaled to the argument size.
+    /// - Parameter size: the size of the image 
     func resize(to size: CGSize) -> CIImage? {
         return self.transformed(by: CGAffineTransform(scaleX: size.width / extent.size.width,
                                                       y: size.height / extent.size.height))
@@ -52,6 +54,9 @@ extension CVPixelBuffer {
 }
 
 /// Creates a SceneKit node with plane geometry, to the argument size, rotation, and material contents.
+/// - Parameters:
+///   - size: size of image
+///   - rotation: rotation of image
 func createPlaneNode(size: CGSize, rotation: Float, contents: Any?) -> SCNNode {
     let plane = SCNPlane(width: size.width, height: size.height)
     plane.firstMaterial?.diffuse.contents = contents
